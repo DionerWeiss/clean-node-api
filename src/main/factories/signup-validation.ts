44a0f2c85e52b78362b47114ca709b54c6e7b65d
DionerWeiss <dionerweiss@gmail.com>
2021-04-17 23:48:01 -0300

    refactor: move required field validation to the ValidationComposite

---
+++ src/main/factories/signup-validation.ts
@@ -0,0 +1,12 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+
+export const makeSignUpValidation = (): Validation => {
+  const validations: Validation[] = []
+
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,8 +4,9 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
-import { Controller } from 'presentation/protocols'
+import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -13,7 +14,8 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
 
   return new LogControllerDecorator(signUpController, logMongoRepository)
