e3b599253914e7020b0f9b75c9caa4f07beb48ed
DionerWeiss <dionerweiss@gmail.com>
2021-04-27 22:04:48 -0300

    refactor: change routes to use filesystem instead fast-glob

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpRequest, HttpResponse } from 'presentation/protocols'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
 
 export class LogControllerDecorator implements Controller {
