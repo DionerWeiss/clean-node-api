dee7a470ac762a30a5ee4f26a1f5bde14a196ee0
DionerWeiss <dionerweiss@gmail.com>
2021-05-26 22:49:24 -0300

    test: ensure DbASaveSurveyResult return a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -64,4 +64,10 @@ describe('DbASaveSurveyResult Usecase', () => {
     const promise = sut.save(makeFakeSurveyResultData())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut()
+    const surveyResult = await sut.save(makeFakeSurveyResultData())
+    expect(surveyResult).toEqual(makeFakeSurveyResult())
+  })
 })
