a28dd59b6df6e65053ce8f4f162cc9f1792a102f
DionerWeiss <dionerweiss@gmail.com>
2021-06-13 21:37:52 -0300

    refactor: add types to AddAccount usecase

--- tests/domain/mocks/mock-account.ts
+++ tests/domain/mocks/mock-account.ts
@@ -1,8 +1,8 @@
 import { AccountModel } from '@/domain/models'
-import { AddAccountParams, AuthenticationParams } from '@/domain/usecases'
+import { AddAccount, AuthenticationParams } from '@/domain/usecases'
 import faker from 'faker'
 
-export const mockAddAccountParams = (): AddAccountParams => ({
+export const mockAddAccountParams = (): AddAccount.Params => ({
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password()
