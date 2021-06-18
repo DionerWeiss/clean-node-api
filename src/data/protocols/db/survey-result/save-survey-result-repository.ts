2e4c22b2794a9dee1257b84c967732804cded9c6
DionerWeiss <dionerweiss@gmail.com>
2021-06-17 21:07:23 -0300

    refactor: add types to SaveSurveyResultRepository

--- src/data/protocols/db/survey-result/save-survey-result-repository.ts
+++ src/data/protocols/db/survey-result/save-survey-result-repository.ts
@@ -1,9 +1,9 @@
 import { SaveSurveyResult } from '@/domain/usecases'
 
 export interface SaveSurveyResultRepository {
-  save (data: LoadSurveyResultRepository.Params): Promise<void>
+  save (data: SaveSurveyResultRepository.Params): Promise<void>
 }
 
-export namespace LoadSurveyResultRepository {
+export namespace SaveSurveyResultRepository {
   export type Params = SaveSurveyResult.Params
 }
