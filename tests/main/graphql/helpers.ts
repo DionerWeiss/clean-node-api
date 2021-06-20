a1bc79c33fa83d8e34eed97b8ca5bc774edcdc93
DionerWeiss <dionerweiss@gmail.com>
2021-06-20 00:16:28 -0300

    test: ensure Login QUery returns an account on valid credentials

---
+++ tests/main/graphql/helpers.ts
@@ -0,0 +1,11 @@
+import typeDefs from '@/main/graphql/type-defs'
+import resolvers from '@/main/graphql/resolvers'
+import schemaDirectives from '@/main/graphql/directives'
+
+import { ApolloServer } from 'apollo-server-express'
+
+export const makeApolloServer = (): ApolloServer => new ApolloServer({
+  resolvers,
+  typeDefs,
+  schemaDirectives
+})
---
+++ tests/main/graphql/login.test.ts
@@ -0,0 +1,58 @@
+import { makeApolloServer } from '@/tests/main/graphql/helpers'
+import { MongoHelper } from '@/infra/db'
+
+import { createTestClient } from 'apollo-server-integration-testing'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
+let apolloServer: ApolloServer
+
+describe('Login GraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = makeApolloServer()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('Login Query', () => {
+    const loginQuery = gql`
+      query login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          accessToken
+          name
+        }
+      }
+    `
+
+    test('Should return an Account on valid credentials', async () => {
+      const password = await hash('123', 12)
+
+      await accountCollection.insertOne({
+        name: 'Dioner',
+        email: 'dioner@gmail.com',
+        password
+      })
+
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'dioner@gmail.com',
+          password: '123'
+        }
+      })
+
+      expect(res.data.login.accessToken).toBeTruthy()
+      expect(res.data.login.name).toBe('Dioner')
+    })
+  })
+})
