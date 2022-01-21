1efe8cc1a67e51ea58deb6a773c408709ea8915f
Dioner <dionerweiss@gmail.com>
2022-01-21 12:29:27 -0300

    refactor: update libs

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -58,7 +58,7 @@ describe('Bcrypt Adapter', () => {
 
     test('Should return false when compare fails', async () => {
       const sut = makeSut()
-      jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(Promise.resolve(false))
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => false)
 
       const isValid = await sut.compare('any_value', 'any_hash')
       expect(isValid).toBe(false)
